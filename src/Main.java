753118481f1d0082ae432d3b810c971b39ec9e73
mycomputer <Danil12.07.2012@gail.com>
2023-01-17 20:40:56 +0300

    Tack1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int [ ] bigBox = new int[3];
+        bigBox [0] = 1;
+        bigBox [1] = 2;
+        bigBox [2] = 3;
+        System.out.println(bigBox[0]);
+        System.out.println(bigBox[1]);
+        System.out.println(bigBox[2]);
+
     }
 }
+
